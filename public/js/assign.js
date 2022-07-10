838b04d1f50a7504be5de20116326d51bae9b5aa
Amrit-Code <59258098+Amrit-Code@users.noreply.github.com>
2022-07-10 13:31:42 +0100

    more efficient

--- public/js/assign.js
+++ public/js/assign.js
@@ -1,18 +1,33 @@
 
+var options = [];
+var amounts = [];
+
 let optSelection = document.getElementById("optSelect");
 
 getVals();
 
 async function getVals(){ 
-	var options = [];
 	
-	await db.collection("questions").get().then((querySnapshot) => {
-			querySnapshot.forEach((doc) => {
-				if (!options.includes(doc.get("Type"))){
-				options.push(doc.get("Type"));
-			}
-			});
+	var Head = await db.collection("questions").doc("Head");
+	
+		await Head.get().then((doc) => {
+		if (doc.exists) {
+			options = doc.get("Types");
+			amounts = doc.get("Amounts");
+		} else {
+			// doc.data() will be undefined in this case
+			console.log("No such document!");
+		}
+		}).catch((error) => {
+			console.log("Error getting document:", error);
 		});
+//	await db.collection("questions").get().then((querySnapshot) => {
+//			querySnapshot.forEach((doc) => {
+//				if (!options.includes(doc.get("Type"))){
+//				options.push(doc.get("Type"));
+//			}
+//			});
+//		});
 	options.forEach(addOptions);
 
 }
--- public/js/myscript.js
+++ public/js/myscript.js
@@ -4,27 +4,69 @@ let questionLabel = document.getElementById("questionLabel");
 
 async function myFunction() {
 	if(document.getElementById("optSelect").value === "All"){
-		var opts = [];
-		await db.collection("questions").get().then((querySnapshot) => {
-			querySnapshot.forEach((doc) => {
-				opts.push(doc.get("question"));
-			});
-		});
-		console.log(opts[Math.floor(Math.random()*opts.length)]);
 		
-		questionLabel.innerHTML = opts[Math.floor(Math.random()*opts.length)];
+		var pos = Math.floor((Math.random()*(options.length-1))+1);
+		var type = options[pos];
+		var amt = amounts[pos];
+		
+		var nextQuestion = "";
+		
+		let question = Math.floor(Math.random()*amt);
+		
+		let padding = 3-(question.toString().length);
+		var fileName = pos.toString() + ("0".repeat(padding)) + question.toString();
+		
+		var ques = await db.collection("questions").doc(fileName);
+	
+		await ques.get().then((doc) => {
+		if (doc.exists) {
+			nextQuestion = doc.get("question");
+		} else {
+			// doc.data() will be undefined in this case
+			console.log("No such document!");
+		}
+		}).catch((error) => {
+			console.log("Error getting document:", error);
+		});
+		
+//		await db.collection("questions").doc(fileName).get().then((querySnapshot) => {
+//			querySnapshot.forEach((doc) => {
+//				nextQuestion = (doc.get("question"));
+//			});
+//		});
+//		console.log(opts[Math.floor(Math.random()*opts.length)]);
+//		
+		questionLabel.innerHTML = nextQuestion;
 		
 	}else{
 	
-	  	var opts = [];
-		await db.collection("questions").where("Type", "==", document.getElementById("optSelect").value).get().then((querySnapshot) => {
-			querySnapshot.forEach((doc) => {
-				opts.push(doc.get("question"));
-			});
-		});
-		console.log(opts[Math.floor(Math.random()*opts.length)]);
+		var type = document.getElementById("optSelect").value;
+	  	var pos = options.indexOf(type);
+		var amt = amounts[pos];
 		
-		questionLabel.innerHTML = opts[Math.floor(Math.random()*opts.length)];
+		
+		var nextQuestion = "";
+		
+		let question = Math.floor(Math.random()*amt);
+		
+		let padding = 3-(question.toString().length);
+		var fileName = pos.toString() + ("0".repeat(padding)) + question.toString();
+
+		
+		var ques = await db.collection("questions").doc(fileName);
+	
+		await ques.get().then((doc) => {
+		if (doc.exists) {
+			nextQuestion = doc.get("question");
+		} else {
+			// doc.data() will be undefined in this case
+			console.log("No such document!");
+		}
+		}).catch((error) => {
+			console.log("Error getting document:", error);
+		});
+		
+		questionLabel.innerHTML = nextQuestion;
 	
 	}
 
